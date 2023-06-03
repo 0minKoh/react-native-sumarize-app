9b41623e2de2f1ad254402d586c9a512a4444970
0minKoh <96913649+0minKoh@users.noreply.github.com>
2023-06-03 10:15:53 +0900

    final_project_v2

--- screens/AppIntro.js
+++ screens/AppIntro.js
@@ -11,19 +11,19 @@ const AppIntro = ({ navigation }) => {
       key: 'slide1',
       // title: 'Welcome to My App',
       // text: 'This is a guide to help you get started with the app.',
-      image: require('../assets/intro_test.png'),
+      image: require('../assets/screen1.png'),
     },
     {
       key: 'slide2',
       // title: 'Features',
       // text: 'Discover all the features of the app and how they can help you.',
-      image: require('../assets/intro_test.png'),
+      image: require('../assets/screen2.png'),
     },
     {
       key: 'slide3',
       // title: 'Get Started',
       // text: 'Create your account and start using the app today.',
-      image: require('../assets/intro_test.png'),
+      image: require('../assets/screen3.png'),
     },
   ];
   const handleDone = () => { // done 버튼을 눌렀을 때 실행됨
@@ -42,9 +42,37 @@ const AppIntro = ({ navigation }) => {
     );
   };
 
+  const renderNextButton = () => {
+    return (
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Next</Text>
+      </View>
+    );
+  };
+
+  const renderDoneButton = () => {
+    return (
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </View>
+    )
+  }
+
+  // skip 버튼 스타일
+  const renderSkipButton = () => {
+    return (
+      <View style={styles.buttonReverse}>
+        <Text style={styles.buttonTextReverse}>Skip</Text>
+      </View>
+    );
+  };
+
   return (
     <AppIntroSlider
       data={slides}
+      renderNextButton={renderNextButton}
+      renderSkipButton={renderSkipButton}
+      renderDoneButton={renderDoneButton}
       renderItem={renderSlide}
       onDone={handleDone}
       showSkipButton={true}
@@ -59,6 +87,26 @@ const styles = StyleSheet.create({
     fontWeight: 700,
     fontSize: 32,
     marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#E31937', // 원하는 색상으로 변경
+    paddingVertical: 12,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  buttonReverse: {
+    backgroundColor: 'white', // 원하는 색상으로 변경
+    paddingVertical: 12,
+    borderRadius: 8,
+  },
+  buttonTextReverse: {
+    color: '#E31937',
+    fontSize: 18,
+    textAlign: 'center'
   },
 })
 
